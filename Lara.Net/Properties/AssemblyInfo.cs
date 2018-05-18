9a4863625d1d731511a7ca9815d1164eeaaa6f55
HardAnonymous <hard25670559@gmail.com>
2018-05-18 16:05:56 +0800

    init

--- Lara.Net/Properties/AssemblyInfo.cs
+++ Lara.Net/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("b877a217-8f22-45f3-b30b-61a730adc13c")]
+[assembly: Guid("a69d3935-bc31-4115-8a10-c600895b33e7")]
 
 // 組件的版本資訊由下列四個值所組成: 
 //
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      修訂編號
 //
 // 您可以指定所有的值，或將組建編號或修訂編號設為預設值
-// 指定為預設值: 
+//方法是使用 '*'，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
